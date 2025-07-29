cf77f4cc71792597ca8166b886e15cf81640fbf3
Yassir Rouane <yasserro100@gmail.com>
2025-07-30 00:38:54 +0100

    feat: shadcn library

---
+++ apps/web/src/pages/Register.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+</script>
+
+<template>
+  <div>
+    <h1>Register</h1>
+    <Button class="cursor-pointer">test</Button>
+  </div>
+</template>
+
+<style scoped></style>
--- apps/web/src/router/index.ts
+++ apps/web/src/router/index.ts
@@ -1,8 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import RegisterPage from '@/pages/Register.vue'
+import LoginPage from '@/pages/Login.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/register',
+      name: 'Register',
+      component: RegisterPage,
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: LoginPage,
+    },
+  ],
 })
 
 export default router
